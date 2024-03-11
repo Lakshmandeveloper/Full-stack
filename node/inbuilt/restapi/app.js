a8cbf716a5188c64f861ba497ca73d96c3dd24ef
lakshmantetali <lakshmantetali@gmail.com>
2024-03-10 23:00:49 -0500

    updated

--- node/inbuilt/restapi/app.js
+++ node/inbuilt/restapi/app.js
@@ -1,31 +1,36 @@
-let express= require('express');
-let app= express();
+let express = require('express');
+let app = express();
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
-const mongoUrl = "mongodb://localhost:27017"
+//const mongoUrl = "mongodb://localhost:27017";
+//const mongoUrl = "mongodb+srv://local:test12345@cluster0.f8vmc.mongodb.net/augintern?retryWrites=true&w=majority"
+const mongoUrl="mongodb+srv://tetali:<tetali123>@cluster0.uy8eapv.mongodb.net/Movies?retryWrites=true&w=majority&appName=Cluster0"
 const dotenv = require('dotenv')
 dotenv.config()
-let port= process.env.PORT || 6788;
+let port = process.env.PORT || 8200;
 var db;
 
-
-app.get('/', (req, res) =>{
-          res.send('WELCOME TO EXPRESS.JS')
+app.get('/',(req,res) => {
+  res.send("Welcome to express")
 })
 
-app.get('/hostel',(req,res) =>{
-     db.collection('hostels').find( ).toArray((err,result)=>{
-      if (err) throw err;
+
+app.get('/hotels',(req,res) => {
+  db.collection('Telugu').find().toArray((err,result) =>{
+      if(err) throw err;
       res.send(result)
-     })
+  })
+})
 
+ MongoClient.connect(mongoUrl, (err,client) => {
+          if(err) console.log("Error While Connecting");
+            db = connection.db('Movies');
+                app.listen(port,( )=>{
+                  console.log(`listening to  port no ${port}`)
+  })
 })
-MongoClient.connect(mongoUrl, (err,connection) =>{
-  if(err) console.log('error while connecting');
-  db = connection.db('hostels');
 
-app.listen(port,( ) =>{
-  console.log(`listening to  the port ${port}`)
-})
-})
+
+
+
 
